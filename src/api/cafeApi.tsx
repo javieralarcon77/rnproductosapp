2246b0cb2f2a469d65a9f85b7bd4d1773ee3c5fb
Javier Alarcon <javier.alarcon@pragma.com.co>
2021-05-04 16:58:05 -0500

    agregado midelware de axios para agregar la cabecera

--- src/api/cafeApi.tsx
+++ src/api/cafeApi.tsx
@@ -1,7 +1,18 @@
 import axios from "axios";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const baseURL = 'https://backend-node-products.herokuapp.com/api';
 
-const cafeApi = axios.create({ baseURL })
+const cafeApi = axios.create({ baseURL });
+
+cafeApi.interceptors.request.use(
+    async ( config )=>{
+        const token = await AsyncStorage.getItem('token');
+        if(token){
+            config.headers['x-token'] = token;
+        }
+        return config;
+    }
+)
 
 export default cafeApi;
