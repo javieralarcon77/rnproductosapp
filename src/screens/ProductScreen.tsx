03942a030c441053f3a34a4dcc0bc66a0065a513
Javier Alarcon <javier.alarcon@pragma.com.co>
2021-05-06 09:23:52 -0500

    agregada recepcion de parametros y cambio de titulo de header

--- src/screens/ProductScreen.tsx
+++ src/screens/ProductScreen.tsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useEffect } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { StackScreenProps } from '@react-navigation/stack';
+import { ProductsStackParams } from '../navigate/ProductsNavigator';
 
-const ProductScreen = () => {
+interface Props extends StackScreenProps<ProductsStackParams,'ProductScreen'>{
+
+}
+
+const ProductScreen = ({ route, navigation }:Props) => {
+    const { id, name = '' } = route.params;
+
+    useEffect(()=>{
+        navigation.setOptions({
+            title: (name !== '') ? name : 'Nuevo Producto'
+        })
+    },[])
+
     return (
         <View>
-            <Text>Producto</Text>
+            <Text style={ styles.text }>Producto: { name }</Text>
+            <Text style={ styles.text }>Id: { id }</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    text:{
+        fontSize: 20,
+        marginVertical: 5,
+    }
+});
 
 export default ProductScreen
